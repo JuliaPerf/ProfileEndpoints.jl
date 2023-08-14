a0651fb85fa8e9d0bad608c16a80fb884756bc86
Nathan Daly <nathan.daly@relational.ai>
2023-08-14 10:30:04 -0600

    Remove type inference profiling, since thread safety was never merged upstream:
    https://github.com/JuliaLang/julia/pull/47615 was never merged, so this
    feature cannot be used yet anyway. :(
    
    And the compat bounds are currently too strict, and are causing issues
    in RelationalAI's build

--- test/runtests.jl
+++ test/runtests.jl
@@ -154,30 +154,6 @@ const url = "http://127.0.0.1:$port"
         end
     end
 
-    @testset "Type inference profiling" begin
-        if !isdefined(Core.Compiler.Timings, :clear_and_fetch_timings)
-            @test HTTP.get("$url/typeinf_profile_start", retry=false, status_exception=false).status == 501
-            @test HTTP.get("$url/typeinf_profile_stop", retry=false, status_exception=false).status == 501
-        else
-            @testset "typeinf start/stop endpoints" begin
-                resp = HTTP.get("$url/typeinf_profile_start", retry=false, status_exception=false)
-                @test resp.status == 200
-                @test String(resp.body) == "Type inference profiling started."
-
-                # workload
-                @eval foo() = 2
-                @eval foo()
-
-                resp = HTTP.get("$url/typeinf_profile_stop", retry=false, status_exception=false)
-                @test resp.status == 200
-                data = read(IOBuffer(resp.body), String)
-                # Test that there's something here
-                # TODO: actually parse the profile
-                @test length(data) > 100
-            end
-        end
-    end
-
     @testset "error handling" begin
         let res = HTTP.get("$url/profile", status_exception=false)
             @test 400 <= res.status < 500
